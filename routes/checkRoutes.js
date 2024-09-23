66c068cee3c956e8a5991af71ddc8139c3f3fbdd
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-23 15:49:20 +0300

    feat: add check toket

---
+++ routes/checkRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { users } = require("../users");
+
+const checkRoutes = express.Router();
+
+checkRoutes.get("", (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) res.status(401).send("Token have not found");
+
+  const token = authorizationHeader.split(" ")[1];
+  jwt.verify(token, "secretKey", (err, result) => {
+    if (err) res.status(403).send("invalid token");
+    const id = result.id;
+    const user =
+      users.find((item) => {
+        return item.id == id;
+      }) || null;
+    if (!user) res.status(403).send("User have not found");
+    req.user = user;
+    console.log("end of checkRoute");
+    next();
+  });
+});
+
+module.exports = {
+  checkRoutes,
+};
